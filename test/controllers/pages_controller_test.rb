fe03047073bfb366e6260480268211add987562e
Jalerson Lima <jalerson@gmail.com>
2017-07-27 11:05:55 -0300

    Ajustando testes.

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,29 +1,7 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
-
-  test "should get what" do
-    get :what
-    assert_response :success
-  end
-
-  test "should get who" do
-    get :who
-    assert_response :success
-  end
-
-  test "should get results" do
-    get :results
-    assert_response :success
-  end
-
-  test "should get contact" do
-    get :contact
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
