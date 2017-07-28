0975019626af544ef20734c4b7e0791d18d2a630
Jalerson Lima <jalerson@gmail.com>
2017-07-28 10:53:34 -0300

    Corrigindo monitoramento do Sentry.

--- config/initializers/setup_sentry.rb
+++ config/initializers/setup_sentry.rb
@@ -1,4 +1,5 @@
 Raven.configure do |config|
-  config.dsn = 'https://9abe61e081b346bfb17bd0d88d3875ad:5f47a12573974f61bff4a54f79d1df02@sentry.io/196621'
+  config.dsn = "https://#{Rails.application.secrets.sentry_key}:#{Rails.application.secrets.sentry_secret}@sentry.io/196621"
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  config.environments = ['production']
 end
