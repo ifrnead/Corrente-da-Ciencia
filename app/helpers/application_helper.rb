d7939c4a24091c3a4417bfda99842184f60f2173
Jalerson Lima <jalerson@gmail.com>
2017-07-28 13:14:12 -0300

    Ajustando responsividade.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,19 @@
 module ApplicationHelper
 
   def home_link
-    "<a href=\"#{ home_path }\" class=\"btn btn-default\"><i class=\"fa fa-home\" ></i><br>Home</a>".html_safe
+    "<a href=\"#{ home_path }\"><i class=\"fa fa-home\"></i> Home</a>".html_safe
   end
 
   def about_link
-    "<a href=\"#{ what_path }\" class=\"btn btn-default\" ><i class=\"fa fa-file-text\"></i><br>Sobre</a>".html_safe
+    "<a href=\"#{ what_path }\"><i class=\"fa fa-file-text\"></i> O que é?</a>".html_safe
   end
 
   def team_link
-    "<a href=\"#{ who_path }\" class=\"btn btn-default\"><i class=\"fa fa-users\"></i><br>Equipe</a>".html_safe
+    "<a href=\"#{ who_path }\"><i class=\"fa fa-users\"></i> Quem somos?</a>".html_safe
   end
 
   def contact_link
-    "<a href=\"#{ contact_path }\" class=\"btn btn-default\"><i class=\"fa fa-envelope\"></i><br>Contato</a>".html_safe
+    "<a href=\"#{ contact_path }\"><i class=\"fa fa-envelope\"></i> Contato</a>".html_safe
   end
 
 end
