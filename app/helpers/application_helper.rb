c7069cfb6e208e226e65d48317af725ccdeb6c70
Jalerson Lima <jalerson@gmail.com>
2017-07-29 11:52:57 -0300

    Adicionando Open Graph tags.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
 
+  def open_graph_meta_tags
+    tags = "<meta property=\"og:url\" content=\"https://ead.ifrn.edu.br/correntedaciencia\" />"
+    tags += "<meta property=\"og:type\" content=\"website\" />"
+    tags += "<meta property=\"og:title\" content=\"Corrente da Ciência\" />"
+    tags += "<meta property=\"og:description\" content=\"O Corrente da Ciência, desenvolvido pelo Campus EaD do IFRN, é um projeto de pesquisa que visa compreender como e quão rápido mensagens se propagam em aplicativos de mensagens e redes sociais.\" />"
+    tags += "<meta property=\"og:image\" content=\"#{image_url('og_image.png')}\" />"
+    tags.html_safe
+  end
+
   def home_link
     "<a href=\"#{ home_path }\"><i class=\"fa fa-home\"></i> Home</a>".html_safe
   end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,7 +2,7 @@ module PagesHelper
 
   URL = "https://ead.ifrn.edu.br/correntedaciencia".freeze
   MENSAGEM_PADRAO = "Participe%20do%20projeto%20Corrente%20da%20Ciência%20você%20também!%20#{URL}".freeze
-  MENSAGEM_TWITTER = "Participe%20do%20projeto%20Corrente%20da%20Ciência%20você%20também!&amp;hashtags=IFRN,TCC,CorrenteDaCiencia".freeze
+  MENSAGEM_TWITTER = "Participe%20do%20projeto%20Corrente%20da%20Ciência%20você%20também!&amp;hashtags=IFRN,EAD,CorrenteDaCiencia".freeze
 
   def facebook_share_link
     "<a href=\"http://www.facebook.com/sharer.php?u=#{URL}\" class=\"btn btn-default share-button\" target=\"_blank\"><i class=\"fa fa-facebook\"></i></a>".html_safe
