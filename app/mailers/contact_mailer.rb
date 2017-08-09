33a7b150e7b231085d4ae6f67fcfe39fbca227e1
Jalerson Lima <jalerson@gmail.com>
2017-08-09 12:02:54 -0300

    Corrigindo configurações de e-mail.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,9 @@
 class ContactMailer < ApplicationMailer
   default to: 'correntedaciencia@ifrn.edu.br'
+  default from: 'correntedaciencia@ifrn.edu.br'
 
   def new_email(message)
     @message = message
-    mail subject: "[Corrente da Ciência] Mensagem de: #{@message.name}", from: message.email
+    mail subject: "[Corrente da Ciência] Mensagem de: #{@message.name}"
   end
 end
